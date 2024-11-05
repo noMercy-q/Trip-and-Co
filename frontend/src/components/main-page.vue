41bc59e56c4d336f4af639cf868986c9e6e642a0
Max Kirillov <max79kirillov@gmail.com>
2024-11-05 19:01:37 +0300

    First steps in Vue

---
+++ frontend/src/components/main-page.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mt-10 ml-10">
+    <v-sheet class="mx-left" width="50%">
+      <v-card>
+        <template #text>{{ text }}</template>
+      </v-card>
+    </v-sheet>
+  </div>
+  </template>
+  
+  <script>
+    import axios from 'axios'
+  
+    export default {
+      data: () => ({
+        text: 'Скоро здесь будет красиво!'
+      }),
+  
+      methods: {
+        async handleNewMessage() {
+          try {
+            const response =
+              await axios.post('our url', { })
+            console.log(response)
+          } catch (error) {
+            this.error = error
+          }
+        }
+      }
+    }
+  </script>
+  
