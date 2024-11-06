bda203003c4984d8103405ca1b98a83a8d339c2e
Mikhail Kornilov <korn-m@yandex-team.ru>
2024-11-06 23:49:07 +0300

    add middleware

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from contextlib import asynccontextmanager
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.routes import router
 from app import db_client
 
@@ -15,8 +17,17 @@ async def lifespan(app: FastAPI):
     await db_client.connect()
     yield
 
+
 app = FastAPI(lifespan=lifespan)
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
