8e0fbc27be1d65dda368690e967bed59bc19a3b7
shamank <russyakrusya@gmail.com>
2024-11-07 01:26:16 +0300

    add: forms

---
+++ frontend/src/components/auth-form.vue
@@ -0,0 +1,175 @@
+<template>
+  <v-container class="d-flex flex-column justify-start align-center " fluid
+               style="min-height: 100vh;">
+
+    <v-img
+        src="assets/logo_white.png"
+        width="auto"
+        max-width="150px"
+        height="100px"
+        alt="Logo"
+
+    ></v-img>
+
+
+    <h1 class="text-center white--text">
+      <span v-if="isLoginForm">Log into your account</span>
+      <span v-else>Register new account</span>
+    </h1>
+
+    <v-card variant="flat" width="350px" class="pa-6 mt-5 rounded-shaped">
+
+      <v-card-text class="mt-2">
+        <v-form>
+          <v-text-field
+              class="mx-auto"
+              density="compact"
+              label="Email address"
+              :rules="rules.emailRules"
+              max-width="300"
+              persistent-placeholder
+              placeholder="email@gmail.com"
+              prepend-inner-icon="mdi-email"
+              variant="outlined"
+              v-model="email"
+
+          ></v-text-field>
+
+          <v-text-field
+              class="mx-auto"
+              density="compact"
+              :rules="rules.passwordRules"
+              max-width="300"
+              persistent-placeholder
+              prepend-inner-icon="mdi-onepassword"
+              label="Password"
+              variant="outlined"
+              dense
+              color="grey"
+              type="password"
+              v-model="password"
+          ></v-text-field>
+
+          <v-text-field
+              v-if="!isLoginForm"
+              :rules="rules.confirmPasswordRules"
+              class="mx-auto"
+              density="compact"
+              max-width="300"
+              persistent-placeholder
+              prepend-inner-icon="mdi-onepassword"
+              label="Confirm Password"
+              variant="outlined"
+              dense
+              color="grey"
+              type="password"
+              v-model="confirmPassword"
+
+          ></v-text-field>
+
+          <v-row class="d-flex align-center justify-space-between mt-4">
+            <v-checkbox label="Remember me" color="blue" v-model="rememberMe" hide-details></v-checkbox>
+            <v-btn variant="plain" text color="primary" class="blue--text text-subtitle-1" @click="forgotPassword">
+              Forgot
+              password?
+            </v-btn>
+          </v-row>
+
+          <v-btn :loading="isSubmitting" class="mt-4 mb-4" block color="blue" dark @click="submit">
+            <span v-if="isLoginForm"> Log In </span>
+            <span v-else>Register</span>
+          </v-btn>
+
+          <v-divider class="my-4"></v-divider>
+
+          <v-row justify="center" class="mt-4">
+            <p v-if="isLoginForm" class="white--text">Don't have an account?
+              <v-btn variant="plain" text="Sign Up" color="blue" @click="toggleForm">Sign up</v-btn>
+            </p>
+            <p v-else class="white--text">Do have an account yet?
+              <v-btn variant="plain" text="Sign Up" color="blue" @click="toggleForm">Log in</v-btn>
+            </p>
+          </v-row>
+        </v-form>
+      </v-card-text>
+
+
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      rememberMe: false,
+      isLoginForm: true,
+      isSubmitting: false,
+      rules: {
+        emailRules: [
+          v => !!v || "E-mail is required",
+          v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+        ],
+        passwordRules: [
+          v => !!v || "Password is required",
+          v => (v && v.length >= 8) || "Password must be at least 8 characters",
+          v => /(?=.*[A-Z])/.test(v) || "Must have one uppercase character",
+          v => /(?=.*\d)/.test(v) || "Must have one number",
+          v => /([!@$%<>*''])/.test(v) || "Must have one special character [!@#$%]"
+        ],
+        confirmPasswordRules: [
+          v => !!v || "Confirm Password is required",
+          v => (v && v.length >= 8) || "Confirm Password must be at least 8 characters",
+          v => v === this.password || "Passwords do not match"
+        ]
+      }
+    };
+  },
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    async submit() {
+      this.isSubmitting = true;
+
+      const endpoint = this.isLoginForm ? "auth/login" : "auth/register"
+
+      try {
+        const response = await axios.post(endpoint, {
+          email: this.email,
+          password: this.password,
+        })
+
+
+        console.log(response.data)
+      } catch (e) {
+        console.log("error on submit: ", e)
+      }
+
+      this.$store.commit('setToken', "token") // TODO: исправить
+
+      this.$router.push("/")
+
+      this.isSubmitting = false;
+
+
+    },
+    toggleForm() {
+      // Логика перехода на страницу регистрации
+      this.isLoginForm = !this.isLoginForm
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+
+</style>
+
+
---
+++ frontend/src/components/create-trip.vue
@@ -0,0 +1,167 @@
+<template>
+  <v-card class="mx-auto rounded-xl" width="75%">
+    <v-card-title class="text-h5 text-center">Create a New Trip</v-card-title>
+    <v-card-text>
+      <v-form ref="tripForm">
+
+        <!-- Название путешествия -->
+        <v-text-field
+            label="Trip Name"
+            density="compact"
+            persistent-placeholder
+            v-model="tripName"
+            :rules="rules.tripNameRules"
+            placeholder="Summer Adventure"
+            prepend-inner-icon="mdi-map"
+            variant="outlined"
+            class="mb-4"
+        ></v-text-field>
+
+        <!-- Города отправления и прибытия на одной строке -->
+        <v-row>
+          <v-col cols="6">
+            <v-autocomplete
+                :items="cityOptions"
+                label="Departure City"
+                density="compact"
+                persistent-placeholder
+                v-model="departureCity"
+                :rules="rules.cityRules"
+                placeholder="New York"
+                prepend-inner-icon="mdi-city"
+                variant="outlined"
+            ></v-autocomplete>
+          </v-col>
+          <v-col cols="6">
+            <v-autocomplete
+                :items="cityOptions"
+                label="Destination City"
+                v-model="destinationCity"
+                density="compact"
+                persistent-placeholder
+                :rules="rules.cityRules"
+                placeholder="Los Angeles"
+                prepend-inner-icon="mdi-city-variant"
+                variant="outlined"
+            ></v-autocomplete>
+          </v-col>
+        </v-row>
+
+        <!-- Даты вылета и прилета на одной строке -->
+        <v-row>
+          <v-col cols="6">
+            <v-text-field
+                label="Departure Date"
+                v-model="startDate"
+                density="compact"
+                persistent-placeholder
+                :rules="rules.dateRules"
+                type="date"
+                prepend-inner-icon="mdi-calendar"
+                variant="outlined"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <v-text-field
+                label="Return Date"
+                density="compact"
+                persistent-placeholder
+                v-model="endDate"
+                :rules="rules.dateRules"
+                type="date"
+                prepend-inner-icon="mdi-calendar-end"
+                variant="outlined"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+      </v-form>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn class="text-none" density="comfortable" color="grey" @click="$emit('close')">Cancel</v-btn>
+      <v-btn class="text-none" density="comfortable" variant="outlined" color="primary" @click="submitTrip"
+             :loading="isSubmitting">Create
+        Trip
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+
+    const today = new Date(); // Форматируем текущую дату в ISO для input типа date
+    const nextWeek = new Date();
+    nextWeek.setDate(today.getDate() + 7)
+
+    return {
+      tripName: '',
+      cities: [],
+      departureCity: '',
+      destinationCity: '',
+      startDate: today.toISOString().slice(0, 10),
+      endDate: nextWeek.toISOString().slice(0, 10),
+      isSubmitting: false,
+      rules: {
+        tripNameRules: [v => !!v || "Trip name is required"],
+        cityRules: [v => !!v || "City is required"],
+        dateRules: [v => !!v || "Date is required"]
+      }
+    };
+  },
+  computed: {
+    cityOptions() {
+      return this.cities.map(city => city.name);
+    },
+  },
+  mounted() {
+    this.fetchCities();
+  },
+  methods: {
+    async fetchCities() {
+      const endpoint = "http://localhost:8000/cities"
+
+      try {
+        const response = await axios.get(endpoint)
+
+        this.cities = response.data || [];
+
+      } catch (e) {
+        console.log("error to fetch cities: ", e)
+      }
+
+
+    },
+    async submitTrip() {
+      this.isSubmitting = true;
+
+      try {
+        // Отправка данных формы
+        const response = await axios.post("/trips", {
+          name: this.tripName,
+          departureCity: this.departureCity,
+          destinationCity: this.destinationCity,
+          startDate: this.startDate,
+          endDate: this.endDate,
+        });
+
+        console.log("Trip created:", response.data);
+
+        // Закрыть диалоговое окно после успешного создания
+        this.$emit('close');
+      } catch (error) {
+        console.error("Error creating trip:", error);
+      } finally {
+        this.isSubmitting = false;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
