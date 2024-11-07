1c7c6095d8dbfdf9bbd356459193f113eb875cc4
SheLesTT <lovsevdenis3513@gmail.com>
2024-11-07 12:48:10 +0300

    rename views

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -6,5 +6,5 @@ router = APIRouter()
 
 router.get("/cities")(cities_handler)
 router.post("/create_trip")(create_trip)
-router.get("/trip_item")(trip_items_handler)
-router.post("/trip_item")(create_trip_item)
+router.get("/views")(trip_items_handler)
+router.post("/create_view")(create_trip_item)
